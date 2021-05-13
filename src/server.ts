206c2c90d2682421f2dcce894a482ad3d46b9028
Júlia Eduarda Miranda de Sousa <juliaeduardasousa@gmail.com>
2021-05-12 23:27:29 -0300

    JSON config and routes adition

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import './database';
+import { routes } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen (3333);
