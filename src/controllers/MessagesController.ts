d912b3bfb5138a8a6c4035ad7e5370bedea9507c
Júlia Eduarda Miranda de Sousa <juliaeduardasousa@gmail.com>
2021-05-13 01:54:57 -0300

    Created controllers

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,30 @@
+import { MessagesService } from "../services/MessagesService";
+import { Request, Response } from "express";
+
+class MessagesController {
+    async create (request: Request, response: Response) {
+        const { admin_id, text, user_id } = request.body;
+
+        const messageService = new MessagesService();
+
+        const message = await messageService.create({
+            admin_id,
+            text,
+            user_id
+        });
+
+        return response.json(message);
+    }
+
+    async showByUser (request: Request, response: Response) {
+        const { id } = request.params;
+
+        const messageService = new MessagesService();
+
+        const list = await messageService.listByUser(id);
+
+        return response.json(list);
+    }
+}
+
+export { MessagesController };
