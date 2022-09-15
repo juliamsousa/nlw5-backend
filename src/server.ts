617c101130131eed4301654f440b8ea2f2d028ba
juliamsousa <juliaeduardasousa@gmail.com>
2022-09-15 19:20:03 -0300

    feat

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import express, { json } from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
 import './database';
 import { routes } from './routes';
 
 const app = express();
+
+// cria protocolo http
+const http = createServer(app);
+// cria ws server
+const io = new Server(http);
 
 app.use(express.json());
 app.use(routes);
